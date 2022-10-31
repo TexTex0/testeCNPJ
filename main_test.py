23020e27bad5f5b011425b1251cea2eaa860e346
TexTex0 <106408687+TexTex0@users.noreply.github.com>
2022-10-31 18:03:47 -0300

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -2,6 +2,6 @@ from main import validate_cnpj
 from gerador_cnpj import generate_cnpj
 
 def testando_cnpj():
-    cnpj = generate_cnpj()
+    cnpj = generate_cnpj()  # PODE DEIXAR GERAR UM CNPJ AUTOMATICAMENTE OU SUBSTITUIR O GENERATE_CNPJ() POR UM CNPJ DA SUA ESCOLHA, MELHORIAS SEMPRE BEM-VINDAS
     assert validate_cnpj(cnpj) == cnpj
     print('\n' + cnpj)
